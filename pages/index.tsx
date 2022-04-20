81014df97dd434b37523cec7ccf58c1977f71e5c
Enguerrand-dev <info@enguerrandvancottem.be>
2022-04-20 18:15:15 +0200

    final build

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,86 +1,77 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import { Quote } from '../typing'
+import { BiRefresh } from 'react-icons/bi'
+import { AiOutlineTwitter } from 'react-icons/ai'
+interface Props {
+  quotes: Quote[]
+}
 
-const Home: NextPage = () => {
+const Home = ({ quotes }: Props) => {
+  const [quote, setQuote] = useState<Quote | null>(null)
+
+  useEffect(() => {
+    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
+    setQuote(randomQuote)
+  }, [quotes])
+
+  const handleQuote = () => {
+    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
+    setQuote(randomQuote)
+  }
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
+    <>
+      <div className="flex min-h-screen flex-col items-center justify-center py-2">
+        <Head>
+          <title>Want a quote ?</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+          <h1 className="mb-10 text-4xl font-bold">U want a quote ?</h1>
+          <div id="quote-box" className="transition">
+            <p id="text" className="text-xl">
+              {quote?.quote}
             </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
+            <span
+              id="author"
+              className="mt-1.5 flex justify-center font-light underline underline-offset-4"
+            >
+              {quote?.author}
+            </span>
+            <div className="mt-10 flex items-center justify-center space-x-5">
+              <button
+                id="new-quote"
+                onClick={handleQuote}
+                className="rounded-3xl bg-blue-500  font-bold text-white transition-colors duration-200 ease-in-out hover:bg-blue-700"
+              >
+                <BiRefresh className="h-7 w-7" />
+              </button>
+              <a
+                id="tweet-quote"
+                target={'_blank'}
+                href={`https://twitter.com/intent/tweet/?text:${quote?.quote}`}
+                className="rounded-3xl bg-blue-500  font-bold text-white transition-colors duration-200 ease-in-out hover:bg-blue-700"
+              >
+                <AiOutlineTwitter className="h-7 w-7 text-white" />
+              </a>
+            </div>
+          </div>
+        </main>
+      </div>
+      <script src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"></script>
+    </>
   )
 }
 
 export default Home
+
+export const getServerSideProps = async () => {
+  const quotes = await fetch(
+    'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+  ).then((res) => res.json())
+  return {
+    props: {
+      ...quotes,
+    },
+  }
+}
